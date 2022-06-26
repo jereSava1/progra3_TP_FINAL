4664142d8b2bf9e3ad3c42d044b5ff022f158936
Facundo Errobidart <facundoerrobidart2001@gmail.com>
2022-06-25 22:52:39 -0300

    (Agus) ticket simplificado

---
+++ src/vista/IVistaGeneral.java
@@ -0,0 +1,12 @@
+package vista;
+
+import java.awt.event.ActionListener;
+
+public interface IVistaGeneral {
+	
+	void mostrar();
+    void esconder();
+    void setActionListener(ActionListener actionListener);
+   
+
+}
